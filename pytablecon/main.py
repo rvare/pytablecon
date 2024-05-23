4f66d8cc817831b4daf7faf434e3ca66890f1fba
Richard Varela <rvarela@csus.edu>
2024-05-22 21:09:49 -0700

    It's terrible, but CSV to MD works.

--- pytablecon/main.py
+++ pytablecon/main.py
@@ -18,8 +18,34 @@ def mdtable_to_csv(lines_temp):
     
     return lines
 
-def csv_to_mdtable(lines_temp):
-    pass
+def csv_to_mdtable(file_name):
+    csv_lines = list()
+    with open(file_name) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        second_line = False
+        temp_str = "|"
+        for line in csv_reader:
+            #print(line)
+            csv_lines.append(line)
+            if second_line == False:
+                for i in csv_lines[0]:
+                    temp_str += "-|"
+                # csv_lines.append(temp_str)
+                temp_str += '\n'
+                second_line = True
+        str_lines = list()
+        second_line = False
+        for i in csv_lines:
+            str_lines.append("| " + " | ".join(i) + " |\n")
+            if second_line == False:
+                str_lines.append(temp_str)
+                second_line = True
+
+    print(csv_lines)
+
+    with open("../tests/test_output.md", mode="w") as output_file:
+        output_file.writelines(str_lines)
+        
 
 if __name__ == '__main__':
     file_name = sys.argv[1]
@@ -39,6 +65,6 @@ if __name__ == '__main__':
             writer.writerows(lines)
     else:
         print("here")
-        lines = csv_to_mdtable(lines_temp)
+        csv_to_mdtable(file_name)
         # with open("../tests/test_out.md", mode="w", newline="") as output_file:
         #     output_file.write(lines)
