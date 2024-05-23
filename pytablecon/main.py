2a58f2d2d8462c06aa307609c0ebef08dae85743
Richard Varela <rvarela@csus.edu>
2024-05-22 20:06:02 -0700

    Made functions and extra test file. Also added to gitignore.

--- pytablecon/main.py
+++ pytablecon/main.py
@@ -1,12 +1,9 @@
 import csv
+import sys
 
-if __name__ == '__main__':
-    lines_temp = list()
-    with open("tests/test1.md") as md_file:
-        for i in md_file:
-            lines_temp.append(i)
-    
+def mdtable_to_csv(lines_temp):
     strip_lines = [i.strip('\n') for i in lines_temp]
+    del strip_lines[1]
     split_lines = [i.split('|') for i in strip_lines]
     lines = list()
     for i in split_lines:
@@ -18,12 +15,30 @@ if __name__ == '__main__':
             i[j] = s.strip()
 
         lines.append(i)
+    
+    return lines
 
-    del lines[1]
+def csv_to_mdtable(lines_temp):
+    pass
 
-    for i in lines:
-        print(i)
+if __name__ == '__main__':
+    file_name = sys.argv[1]
+    lines_temp = list()
+    # with open("../tests/test1.md") as md_file:
+    with open(file_name) as md_file:
+        for i in md_file:
+            lines_temp.append(i)
 
-    with open("tests/test.csv", mode="w", newline="") as output_file:
-        writer = csv.writer(output_file)
-        writer.writerows(lines)
+    file_extension = file_name.split('.')[3]
+    print(file_extension)
+
+    if file_extension == "md":
+        lines = mdtable_to_csv(lines_temp)
+        with open("../tests/test_output.csv", mode="w", newline="") as output_file:
+            writer = csv.writer(output_file)
+            writer.writerows(lines)
+    else:
+        print("here")
+        lines = csv_to_mdtable(lines_temp)
+        # with open("../tests/test_out.md", mode="w", newline="") as output_file:
+        #     output_file.write(lines)
