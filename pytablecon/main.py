c7b626e9ed157402757fafe86c6d8a0844aa788a
Richard Varela <rvarela@csus.edu>
2024-05-29 22:07:54 -0500

    MD table to TSV works as should.

--- pytablecon/main.py
+++ pytablecon/main.py
@@ -95,6 +95,44 @@ def md_alignment_columns(cols, delimiter):
     
     return alignment_columns
 
+def mdtable_to_tsv(file_name):
+    """
+    Convert Markdown table to TSV.
+    TODO: Handle different kinds of Markdown tables.
+    """
+    # Get Markdown table
+    md_lines = list()
+    with open(file_name) as md_file:
+        for i in md_file:
+            md_lines.append(i)
+
+    # Get Markdown table
+    md_lines = list()
+    with open(file_name) as md_file:
+        for i in md_file:
+            md_lines.append(i)
+
+    # Process Markdown table and convert
+    processed_lines = [i.strip('\n').split('|') for i in md_lines]
+    del processed_lines[1]
+
+    str_lines = list()
+    for i in processed_lines:
+        if i[0] == '' and i[-1] == '':
+            del i[0]
+            del i[-1]
+        # Remove white spaces in string
+        for j, s in enumerate(i):
+            i[j] = s.strip()
+        str_lines.append(i)
+
+    joined_str = list()
+    for i in str_lines:
+        joined_str.append("\t".join(i) + '\n')
+    
+    with open("../tests/test_output.tsv", "w") as output_file:
+        output_file.writelines(joined_str)
+
 if __name__ == '__main__':
     file_name = sys.argv[1]
     lines_temp = list()
@@ -105,7 +143,8 @@ if __name__ == '__main__':
     # print(file_name.split('.'))
 
     if file_extension == "md":
-        mdtable_to_csv(file_name)
+        #mdtable_to_csv(file_name)
+        mdtable_to_tsv(file_name)
     elif file_extension == "csv":
         csv_to_mdtable(file_name)
     elif file_extension == "tsv":
