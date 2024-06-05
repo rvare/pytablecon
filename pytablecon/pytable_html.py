0120c67bf341b68edf9675f5183066223d26ca66
Richard Varela <rvarela@csus.edu>
2024-06-05 09:26:14 -0500

    Defined threefunctions that convert to HTML. They are just the headers for now and will write the bodies soon. Just wanted to at least have a starting commit to work with.

---
+++ pytablecon/pytable_html.py
@@ -0,0 +1,11 @@
+# Contains functions that go from one format to HTML
+import csv
+
+def csv_to_html(file_name):
+    pass
+
+def tsv_to_html(file_name):
+    pass
+
+def md_to_html(file_name):
+    pass
