f26301294a090decba25dd4b3c76d06358f575de
Richard Varela <richardvare13@gmail.com>
2026-03-21 10:58:24 -0700

    Created pytable_constants.py that will contain any constants that the program needs. As of right now, it includes string contants that represents file extensions. Also modified main.py to use the constants.

--- pytablecon/main.py
+++ pytablecon/main.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import pytable_constants as ptc
 from pytable_csv import *
 from pytable_tsv import *
 from pytable_md import *
@@ -18,30 +19,30 @@ if __name__ == '__main__':
     file_name: str = sys.argv[2]
     file_extension: str = sys.argv[2].split('.')[-1] # Grabs the file extension
 
-    if file_extension not in ("csv", "tsv", "md"):
+    if file_extension not in (ptc.CSV_FILE_EXTENSION, ptc.TSV_FILE_EXTENSION, ptc.MARKDOWN_FILE_EXTENSION):
         print("Error: your file must have the following extensions: csv, tsv, or md")
         exit()
 
     if args.csv:
-        if file_extension == "tsv":
+        if file_extension == ptc.TSV_FILE_EXTENSION:
             tsv_to_csv(file_name)
-        elif file_extension == "md":
+        elif file_extension == ptc.MARKDOWN_FILE_EXTENSION:
             mdtable_to_csv(file_name)
     elif args.tsv:
-        if file_extension == "csv":
+        if file_extension == ptc.CSV_FILE_EXTENSION:
             csv_to_tsv(file_name)
-        elif file_extension == "md":
+        elif file_extension == ptc.MARKDOWN_FILE_EXTENSION:
             md_to_tsv(file_name)
     elif args.html:
-        if file_extension == "csv":
+        if file_extension == ptc.CSV_FILE_EXTENSION:
             csv_to_html(file_name)
-        elif file_extension == "tsv":
+        elif file_extension == ptc.TSV_FILE_EXTENSION:
             tsv_to_html(file_name)
-        elif file_extension == "md":
+        elif file_extension == ptc.MARKDOWN_FILE_EXTENSION:
             md_to_html(file_name)
     elif args.markdown:
-        if file_extension == "csv":
+        if file_extension == ptc.CSV_FILE_EXTENSION:
             csv_to_mdtable(file_name)
-        elif file_extension == "tsv":
+        elif file_extension == ptc.TSV_FILE_EXTENSION:
             tsv_to_mdtable(file_name)
 
